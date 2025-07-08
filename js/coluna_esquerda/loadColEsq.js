27f8a55b83b8c06598cd815904ada1782eb16a4a
renan tomazini <renantomazini@gmail.com>
2025-07-08 18:41:39 -0300

    remove

--- js/coluna_esquerda/loadColEsq.js
+++ js/coluna_esquerda/loadColEsq.js
@@ -2,14 +2,14 @@
     const pastaPosts = "/posts/";
     const container = document.querySelector('.coluna-esquerda');
 
-    // Lista manual ou via JSON no futuro (por enquanto, hardcode para exemplo)
+   
     const arquivos = [
 
       "teste2.html",
       "teste.html",
       "olamundo.html",
       "vermais.html"
-    ]; // Ordem decrescente (últimos primeiro)
+    ]; 
 
     const ul = document.createElement('ul');
 
@@ -18,7 +18,7 @@
         const res = await fetch(pastaPosts + arquivo);
         const texto = await res.text();
 
-        // Extrair o <title> usando DOMParser
+
         const doc = new DOMParser().parseFromString(texto, "text/html");
         const titulo = doc.querySelector('title')?.textContent || arquivo;
 
